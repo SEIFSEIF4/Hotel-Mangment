1848868b01f8ffe074282b5c164bffc54035e8b0
Seif Elesllam <seifsaif151@gmail.com>
2024-05-04 19:54:09 +0300

    first commit

---
+++ src/data/data-activity.js
@@ -0,0 +1,52 @@
+export const activity = [
+  {
+    id: 2001,
+    status: "confirmed",
+    numNights: 7,
+  },
+  {
+    id: 305,
+    status: "unconfirmed",
+    numNights: 3,
+  },
+  {
+    id: 789,
+    status: "confirmed",
+    numNights: 4,
+  },
+  {
+    id: 402,
+    status: "unconfirmed",
+    numNights: 6,
+  },
+  {
+    id: 611,
+    status: "confirmed",
+    numNights: 2,
+  },
+  {
+    id: 111,
+    status: "confirmed",
+    numNights: 2,
+  },
+  {
+    id: 222,
+    status: "unconfirmed",
+    numNights: 5,
+  },
+  {
+    id: 333,
+    status: "confirmed",
+    numNights: 3,
+  },
+  {
+    id: 444,
+    status: "unconfirmed",
+    numNights: 6,
+  },
+  {
+    id: 555,
+    status: "confirmed",
+    numNights: 1,
+  },
+];
---
+++ src/features/dashboard/Stats.jsx
@@ -0,0 +1,61 @@
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+import Stat from "./Stat";
+import { formatCurrency } from "../../utils/helpers";
+
+function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
+  // 1.
+  const numBookings = bookings.length;
+
+  // 2.
+  const sales = bookings.reduce((acc, cur) => acc + cur.totalPrice, 0);
+
+  // 3.
+  const checkins = confirmedStays.length;
+
+  // 4.
+  const occupation =
+    confirmedStays.reduce((acc, cur) => acc + cur.numNights, 0) /
+    (numDays * cabinCount);
+  // num checked in nights / all available nights (num days * num cabins)
+  // commented out variables to save db calls
+
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        // value={numBookings}
+        value={100}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        // value={formatCurrency(sales)}
+        value={"$100000"}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        // value={checkins}
+        value={10}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        // value={Math.round(occupation * 100) + "%"}
+        value={50 + "%"}
+      />
+    </>
+  );
+}
+
+export default Stats;
